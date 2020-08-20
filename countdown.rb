9d279ee4547bb148e9fb400a148cd0089ce7481b
Jason Markson <jasonmarkson76@gmail.com>
2020-08-20 22:52:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown (num)
-  while num >= 1
-    puts "#{num} SECOND(S)!"
-    num -= 1
+def countdown (number)
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
